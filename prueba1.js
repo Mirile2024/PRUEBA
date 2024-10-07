7335c2f17a7c07debb425c7b06bda96cba77bd12
Mirile2024 <mirileas@gmail.com>
2024-10-07 14:00:26 -0300

    con marcado de codigo para control

---
+++ prueba1.js
@@ -0,0 +1,48 @@
+function cargarDatosDesdeServidor(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            const exito = true;
+            if (exito){
+                const datos= ['dato1', 'dato2', 'dato3'];
+                resolve(datos);
+//--desde aca --------------------
+                const API_URL = 'https://swapi.dev/api/people/?page=2&format=json'
+
+const obtenerPersonajes = async () => {
+    const respuesta = await fetch(API_URL)
+    const personajes = await respuesta.json()
+    mostrarPersonajes(personajes)
+    console.log(personajes);
+}
+const mostrarPersonajes = personajes => {
+    const lista = document.getElementById('lista-personajes')
+    //--------siempre ver jerarquia de las APIS
+    personajes.results.forEach(personaje => {
+        const personajesName = document.createElement('article')
+        personajesName.classList.add('personaje')
+        personajesName.innerHTML = `
+     <h3>${personaje.name}<h3/>
+     <p>height: ${personaje.height}</p>
+     <p>${personaje.gender}</p>
+     `
+        lista.appendChild(personajesName)
+    })
+}
+
+obtenerPersonajes()
+//------Hasta aca----------------Donde esta bien posicionado el codigo ? aca o en la prueba2.js
+
+            }else{
+                reject (new Error('Error al cargar datos'));
+            }
+            }, 2000);
+    })
+}
+cargarDatosDesdeServidor()
+.then((datos)=>{
+    console.log(datos,'Datos cargados exitosamente');
+    
+})
+.catch ((error)=>{
+    console.log(error.message);
+});
